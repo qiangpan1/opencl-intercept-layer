d3684d6652244134e5cd93077ec430261d40388d
Qiang Pan <qiang.pan@intel.com>
2024-04-03 11:32:57 +0800

    add comment

--- release/install.bat
+++ release/install.bat
@@ -3,7 +3,8 @@
 @if exist IntelOCLEtw.dll copy /Y IntelOCLEtw.dll "%ProgramFiles%\Intel\Graphics"
 @echo Completed the Installation...
 
-@setx  OCL_API_STICKER_FILTER "clEnqueueNDRangeKernel,clCreateBuffer,clEnqueueCopyBuffer,clEnqueueReadBuffer,clEnqueueReadBuffer,clEnqueueMapBuffer,clCreateSubBuffer,clEnqueueReadBufferRect,clEnqueueWriteBuffer,clEnqueueWriteBufferRect,clEnqueueFillBuffer,clEnqueueCopyBufferRect,clCreateImage,clEnqueueReadImage,clEnqueueWriteImage,clEnqueueFillImage,clEnqueueCopyImage, clEnqueueCopyImageToBuffer ,clEnqueueCopyBufferToImage, clEnqueueMapImage"
+@setx  OCL_API_STICKER_FILTER "clCreateKernel,clCreateSubBuffer,clEnqueueReadBufferRect,clEnqueueWriteBuffer,clEnqueueWriteBufferRect,clEnqueueFillBuffer,clEnqueueCopyBufferRect,clCreateImage,clEnqueueReadImage,clEnqueueWriteImage,clEnqueueFillImage,clEnqueueCopyImage, clEnqueueCopyImageToBuffer ,clEnqueueCopyBufferToImage, clEnqueueMapImage,clSetKernelArg"
 @setx  OCL_API_STICKER_FILTER_NEGATIVE "clSetKernelArg,"
+REM clEnqueueNDRangeKernel,clCreateBuffer,clEnqueueMapBuffer,clEnqueueReadBuffer,clEnqueueCopyBuffer,clReleaseKernel,clReleaseMemObject,clRetainKernel,clRetainMemObject,clSetKernelArg
 @setx  OCL_TRACE_FILTER ""
 @echo Completed the config...
